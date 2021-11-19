1153680458506938034b6ee1e843c8b9dfc52328
Tridius1 <tristan.t73@gmail.com>
2021-11-19 12:28:21 -0700

    Working nominally

--- js/main.js
+++ js/main.js
@@ -5,10 +5,22 @@
 async function spaceEvent(event) {
 	if (event.code === 'Space') {
     console.log('Space pressed');
-    await drawS();
+    if (sDown) {
+    	// Already pressed
+    } else {
+    	// Fresh push
+    	sDown = true;
+    	await doDraw();
+    }
   }
 }
 
+async function noSpaceEvent(event) {
+	if (event.code === 'Space') {
+    console.log('Space released');
+    sDown = false;
+  }
+}
 
 // Active Functions
 
@@ -28,14 +40,7 @@ function drawBlank() {
 	}
 }
 
-// Async Functions
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-async function drawS() {
+function drawS() {
 	// on space event
 	var canvas = document.getElementById('interactive0');
 	if (canvas.getContext) {
@@ -45,17 +50,41 @@ async function drawS() {
 		var Y = canvas.height / 3;
     ctx.fillRect(X, Y, X, Y);
     console.log('Square drawn');
-    await sleep(190);
-    drawBlank();
-
 	} else {
 		// Canvas not supported by browser
 		console.log('Canvas not supported by browser');
 	}
 }
 
+// Async Functions
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function doDraw() {
+	let s = -1;
+	if (count < 7) {
+		// Delay
+		s = 150;
+	} else {
+		// No Delay
+		s = 0;
+	}
+	await sleep(s);
+	drawS();
+	await sleep(300);
+	drawBlank();
+	count = count + 1;
+	console.log(s.toString() + 'ms delay, ' + count.toString() + ' draws');
+}
+
 
 // Event Listener
-document.addEventListener('keyup', spaceEvent);
+document.addEventListener('keydown', spaceEvent);
+document.addEventListener('keyup', noSpaceEvent);
+
+let count = 0; // # of times doDraw has been called
+let sDown = false; // is the spacebar down?
 
 
